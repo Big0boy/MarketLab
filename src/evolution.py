b7a24ef0410a04570b824ba5238a945cd4c7817f
Sparsh Kumbhare <sparshkumbhare21@gmail.com>
2026-03-16 15:37:13 +0530

    Revert "Phase 1 complete"
    This reverts commit e8291c34b13bf407b4d7d6bd53907a8d30c5953e.

---
+++ src/evolution.py
@@ -0,0 +1,11 @@
+import simulation as sim
+import models as m
+import loader
+
+def _Fitness(agent):
+    config =  
+    returns = (agent.get_total_wealth() - )
+
+
+def ArrangeByFitness(agents : list):
+    pass
